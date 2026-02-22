683ae8ed10fa7ed9179457efd7bd77f7e0031e04
unknown <sadachhena@gmail.com>
2026-02-22 01:32:51 -0800

    Data Add

---
+++ src/Components/Data/Data.jsx
@@ -0,0 +1,200 @@
+export let data=[
+    {
+        id:"1",
+        Name:"Stawberry",
+        Image:"https://images.unsplash.com/photo-1601004890684-d8cbf643f5f2?q=80&w=415&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Category:"Fruits"
+
+    },
+    {
+        id:"2",
+        Name:"Orange",
+        Image:"https://images.unsplash.com/photo-1557800636-894a64c1696f?q=80&w=465&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Category:"Fruits"
+
+    },
+    {
+        id:"3",
+        Name:"Banana",
+        Image:"https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Category:"Fruits"
+
+    },
+    {
+        id:"4",
+        Name:"PineApple",
+        Image:"https://images.unsplash.com/photo-1550258987-190a2d41a8ba?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Category:"Fruits"
+
+    },
+    {
+        id:"5",
+        Name:"Grapis",
+        Image:"https://images.unsplash.com/photo-1615484476889-2830f980a5e3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Category:"Fruits"
+
+    },
+    {
+        id:"6",
+        Name:"",
+        Image:"",
+        Category:"Fruits"
+
+    },
+    {
+        id:"6",
+        Name:"Cherry",
+        Image:"https://images.unsplash.com/photo-1559181567-c3190ca9959b?q=80&w=464&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Category:"Fruits"
+
+    },
+    {
+        id:"8",
+        Name:"Tomato",
+        Image:"https://images.unsplash.com/photo-1662370761575-05ff1ee40d7d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dmVnZXRhYmxlc3xlbnwwfHwwfHx8MA%3D%3D",
+        Category:"Vegetables"
+
+    },
+     {
+        id:"9",
+        Name:"Carrot",
+        Image:"https://images.unsplash.com/photo-1590868309235-ea34bed7bd7f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHZlZ2V0YWJsZXN8ZW58MHx8MHx8fDA%3D",
+        Category:"Vegetables"
+
+    },
+     {
+        id:"10",
+        Name:"Catrrot",
+        Image:"https://images.unsplash.com/photo-1518843875459-f738682238a6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHZlZ2V0YWJsZXN8ZW58MHx8MHx8fDA%3D",
+        Category:"Vegetables"
+
+    },
+     {
+        id:"11",
+        Name:"Carrot",
+        Image:"https://images.unsplash.com/photo-1526470303-82c787d88682?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHZlZ2V0YWJsZXN8ZW58MHx8MHx8fDA%3D",
+        Category:"Vegetables"
+
+    },
+     {
+        id:"12",
+        Name:"Carrt",
+        Image:"https://plus.unsplash.com/premium_photo-1675366071307-4be5bda2ceda?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHZlZ2V0YWJsZXN8ZW58MHx8MHx8fDA%3D",
+        Category:"Vegetables"
+
+    },
+     {
+        id:"13",
+        Name:"Carrot",
+        Image:"https://plus.unsplash.com/premium_photo-1667049290968-d0e2b9c36e01?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8dmVnZXRhYmxlc3xlbnwwfHwwfHx8MA%3D%3D",
+        Category:"Vegetables"
+
+    },
+     {
+        id:"14",
+        Name:"Carrot",
+        Image:"https://images.unsplash.com/photo-1597362925123-77861d3fbac7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dmVnZXRhYmxlc3xlbnwwfHwwfHx8MA%3D%3D",
+        Category:"Vegetables"
+
+    },
+     {
+        id:"15",
+        Name:"Meat",
+        Image:"https://images.unsplash.com/photo-1607623814075-e51df1bdc82f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8TWVhdHxlbnwwfHwwfHx8MA%3D%3D",
+        Category:"Meat"
+
+    },
+     {
+        id:"16",
+        Name:"Meat",
+        Image:"https://images.unsplash.com/photo-1603048297172-c92544798d5a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8TWVhdHxlbnwwfHwwfHx8MA%3D%3D",
+        Category:"Meat"
+
+    },
+     {
+        id:"17",
+        Name:"Meat",
+        Image:"https://plus.unsplash.com/premium_photo-1668616815670-3755f5ddee40?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fE1lYXR8ZW58MHx8MHx8fDA%3D",
+        Category:"Meat"
+
+    },
+     {
+        id:"18",
+        Name:"Meat",
+        Image:"https://images.unsplash.com/photo-1602470520998-f4a52199a3d6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1lYXR8ZW58MHx8MHx8fDA%3D",
+        Category:"Meat"
+
+    },
+     {
+        id:"18",
+        Name:"Meat",
+        Image:"https://images.unsplash.com/photo-1690983325551-b922137727be?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8TWVhdHxlbnwwfHwwfHx8MA%3D%3D",
+        Category:"Meat"
+
+    },
+     {
+        id:"19",
+        Name:"Meat",
+        Image:"https://images.unsplash.com/photo-1690983323238-0b91789e1b5a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fE1lYXR8ZW58MHx8MHx8fDA%3D",
+        Category:"Meat"
+
+    },
+     {
+        id:"20",
+        Name:"Meat",
+        Image:"https://images.unsplash.com/photo-1558030006-450675393462?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fE1lYXR8ZW58MHx8MHx8fDA%3D",
+        Category:"Meat"
+
+    },
+     {
+        id:"21",
+        Name:"Butter",
+        Image:"https://plus.unsplash.com/premium_photo-1700440539073-c769891a9e3f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8QnV0dGVyfGVufDB8fDB8fHww",
+        Category:"Butter"
+
+    },
+     {
+        id:"22",
+        Name:"Butter",
+        Image:"https://plus.unsplash.com/premium_photo-1700887028603-53b13f06233c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fEJ1dHRlcnxlbnwwfHwwfHx8MA%3D%3D",
+        Category:"Butter"
+
+    },
+     {
+        id:"23",
+        Name:"Butter",
+        Image:"https://images.unsplash.com/photo-1603596310923-dbb12732f9c7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fEJ1dHRlcnxlbnwwfHwwfHx8MA%3D%3D",
+        Category:"Butter"
+
+    },
+     {
+        id:"24",
+        Name:"Butter",
+        Image:"https://images.unsplash.com/photo-1573812461383-e5f8b759d12e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fEJ1dHRlcnxlbnwwfHwwfHx8MA%3D%3D",
+        Category:"Butter"
+
+    },
+     {
+        id:"25",
+        Name:"Butter",
+        Image:"https://images.unsplash.com/photo-1587185717368-4d92f8de4ad2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8QnV0dGVyfGVufDB8fDB8fHww",
+        Category:"Butter"
+
+    },
+     {
+        id:"26",
+        Name:"Butter",
+        Image:"https://images.unsplash.com/photo-1589985269102-ff38adf6f00d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8QnV0dGVyfGVufDB8fDB8fHww",
+        Category:"Butter"
+
+    },
+     {
+        id:"27",
+        Name:"Butter",
+        Image:"https://images.unsplash.com/photo-1589985270826-4b7bb135bc9d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8QnV0dGVyfGVufDB8fDB8fHww",
+        Category:"Butter"
+
+    },
+    
+    
+]
--- src/Components/Tab/Tab.jsx
+++ src/Components/Tab/Tab.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Tab() {
+  let [tab,settab]=useState("ALL");
+  let Array=["ALL",'Fruits','Vegetables','Meat','Butter']
   return (
-    <section>
-        <h1 className='text-center font-semibold text-3xl py-5'>TABS</h1>
+    <section className='flex mx-auto max-w-[1400px] px-10 py-10 gap-5 justify-center items-center'>
+        
+        {
+          Array.map(
+            (Items,i)=>{
+              return(
+                <div className=' '>
+                  <button
+                  onClick={()=>settab(Items)}
+                  className={`p-3 font-bold  rounded-lg ${tab===Items ? 'text-white bg-gradient-to-tr from-red-500 to-red-700 ' : "bg-zinc-200 text-black"} `}>{Items}</button>
+                </div>
+              )
+            }
+          )
+        }
     </section>
   )
 }
